f71503bae525f22647faa70feefbd87a0aa9bd83
Juan Cruz Donato <juandonato@gmail.com>
2020-10-25 20:02:38 -0300

    Styled create listing page

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,10 +1,23 @@
 from django.forms import ModelForm, HiddenInput
 from .models import Listing, Bid, Comment
+from django.forms.widgets import TextInput, Textarea, NumberInput, URLInput
 
 class ListingForm(ModelForm):
     class Meta:
         model = Listing
-        fields = ['title', 'description', 'startingPrice', 'imageLink', 'category']
+        fields = ['title', 'description', 'imageLink', 'category', 'startingPrice']
+        labels = {"title": "", "description": "", "imageLink": "", "startingPrice": ""}
+        widgets = {
+            'title': TextInput(attrs={  'placeholder': 'Title',
+                                        'id': 'listingFormTitle'}),
+            'description': Textarea(attrs={ 'placeholder': 'Type the auction description here',
+                                            'id': 'listingFormDescription'}),
+            'imageLink': URLInput(attrs={   'placeholder': 'Image URL Link',
+                                            'id': 'listingFormImageLink'}),
+            'startingPrice': NumberInput(attrs={'placeholder': 'Starting Price',
+                                                'id': 'listingFormStartingPrice'}),
+        }
+
 
 class BidForm(ModelForm):
     class Meta:
